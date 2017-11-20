f84971a7158c750cffeacd695a1845b0369f2d89
tongxindao <balin1991@qq.com>
2017-11-21 00:58:00 +0800

    temp commit

--- shiyanlou_plus/day_04/8_multiprocessing_queue.py
+++ shiyanlou_plus/day_04/8_multiprocessing_queue.py
@@ -1,24 +1,39 @@
-from multiprocessing import Process, Queue
-
-queue = Queue()
+from multiprocessing import Process, Queue, current_process
 
 
 def f1(queue):
-    queue.put("Hello shiyanlou")
+    try:
+        # if not queue.full():
+        shiyanlou = ["Hello", "shiyanlou"]
+        queue.put(shiyanlou)
+        print("f1 pid {0}, f1 qsize {1}, f1 data is: {2}".format(
+            current_process().name, queue.qsize(), shiyanlou))
+    except:
+        print("full")
+        queue.close()
 
 
 def f2(queue):
-    while queue.empty():
-        data = queue.get()
-        print(data)
+    try:
+        # if not queue.empty():
+        for i in range(queue.qsize()):
+            print("f2 pid {0}, qsize is {1}".format(current_process().name, queue.qsize()))
+            print("f2 data is: {0}".format(queue.get()))
+            i += 1
+    except:
+        print("empty")
+        queue.close()
 
 
 def main():
     queue = Queue()
+
     p1 = Process(target=f1, args=(queue, ))
     p2 = Process(target=f2, args=(queue, ))
+
     p1.start()
     p2.start()
+
     p1.join()
     p2.join()
 
--- shiyanlou_plus/day_04/calculator.py
+++ shiyanlou_plus/day_04/calculator.py
@@ -4,56 +4,49 @@ import os
 import sys
 import csv
 import time
-import random
+import queue
 import getopt
+import random
+import multiprocessing
 
 from decimal import Decimal
-from multiprocessing import Process, Queue
 
 
 def process_config(config_file):
-    social_security_percent = {}
-
     try:
-        if(os.path.exists(config_file)):
-            with open(config_file, "r") as config:
-                for line in config:
-                    item = line.split("=")[0].strip()
-                    number = line.split("=")[1].strip()
-                    social_security_percent[item] = number
-                return social_security_percent
-    except FileNotFoundError as e:
-        print("config file not found: {0}".format(e))
+        with open(config_file, "r") as config:
+            social_security_percent = {}
+            for line in config:
+                item = line.split("=")[0].strip()
+                number = line.split("=")[1].strip()
+                social_security_percent[item] = number
+            return social_security_percent
+    except BaseException as e:
+        print("process_config func Exception: {0}".format(e))
         sys.exit(0)
 
 
 def process_data(data_file, queue_for_get_calc_data):
     try:
-        if(os.path.exists(data_file)):
-            salary_data = []
-            with open(data_file, "r") as data:
-                for job_number, salary in csv.reader(data, delimiter=","):
-                    salary_data.append(job_number) 
-                    salary_data.append(salary)
-                    queue_for_get_calc_data.put(salary_data)
-                print("process data process id is: {0}, data is: {1}".format(os.getpid(), salary_data))
+        with open(data_file, "r") as data:
+            for job_number, salary in csv.reader(data, delimiter=","):
+                salary_data = []
+                salary_data.append(job_number)
+                salary_data.append(salary)
+                queue_for_get_calc_data.put_nowait(salary_data)
+                salary_data = []
     except BaseException as e:
-        print("data file not found: {0}".format(e))
+        print("process_data func Exception: {0}".format(e))
         sys.exit(0)
 
 
 def parsing_parameter(argv):
-    social_security_percent = {}
-    data_file = ""
-    output_file = ""
-
     try:
         opts, args = getopt.getopt(
             argv, "c:d:o:", [
                 "config=", "data=", "output="])
     except getopt.GetoptError as e:
-        print(
-            "{0}\n\'./calculator.py -c <cfg> -d <src> -o <dst>\'".format(e))
+        print("{0}\n\'./calculator.py -c <cfg> -d <src> -o <dst>\'".format(e))
         sys.exit(0)
 
     try:
@@ -66,7 +59,7 @@ def parsing_parameter(argv):
                 output_file = arg
         return social_security_percent, data_file, output_file
     except BaseException as e:
-        print("Please confirm your parameter: {0}".format(e))
+        print("parsing_parameter func Exception: {0}".format(e))
         sys.exit(0)
 
 
@@ -78,20 +71,12 @@ def get_salary(salary):
         else:
             raise
     except BaseException as e:
-        print("Salary must be lagger than zero: {0}".format(e))
+        print("get_salary func Exception: {0}".format(e))
         sys.exit(0)
 
 
-def cal_social_security(
-        baselow,
-        basehigh,
-        pension,
-        medical,
-        unemployment,
-        injury,
-        matermity,
-        provident,
-        salary):
+def cal_social_security(baselow, basehigh, pension, medical,
+                        unemployment, injury, matermity, provident, salary):
 
     social_security_tax = pension + medical \
         + unemployment + injury + matermity + provident
@@ -109,6 +94,7 @@ def cal_social_security(
 def cal_taxable_income(salary, social_security):
     start_point = 3500
     taxable_income = 0
+
     taxable_income = salary - social_security - start_point
 
     if taxable_income <= 0:
@@ -142,89 +128,62 @@ def after_salary_tax(salary, social_security, tax):
     return after_tax_salary
 
 
+def read_from_queue(social_security_percent,
+                    queue_for_get_calc_data, queue_for_calc_write_data):
+
+    baselow = float(social_security_percent["JiShuL"])
+    basehigh = float(social_security_percent["JiShuH"])
+    pension = float(social_security_percent["YangLao"])
+    medical = float(social_security_percent["YiLiao"])
+    unemployment = float(social_security_percent["ShiYe"])
+    injury = float(social_security_percent["GongShang"])
+    matermity = float(social_security_percent["ShengYu"])
+    provident = float(social_security_percent["GongJiJin"])
+
+    try:
+        while True:
+            before_salary_data = queue_for_get_calc_data.get()
+            print("queue_for_get_calc_data.get: {0}".format(before_salary_data))
+            job_number = before_salary_data[0]
+            before_salary = before_salary_data[1]
+
+            salary = get_salary(before_salary)
+            social_security = cal_social_security(baselow, basehigh, pension,
+                                                  medical, unemployment,
+                                                  injury, matermity, provident,
+                                                  salary)
+            taxable_income = cal_taxable_income(salary, social_security)
+            tax = calculator_tax(taxable_income)
+            after_salary = after_salary_tax(salary, social_security, tax)
+
+            social_security = str(
+                Decimal(social_security).quantize(
+                    Decimal(".01")))
+            tax = str(Decimal(tax).quantize(Decimal(".01")))
+            after_salary = str(Decimal(after_salary).quantize(Decimal(".01")))
+
+            output_info = [
+                job_number,
+                salary,
+                social_security,
+                tax,
+                after_salary]
+            queue_for_calc_write_data.put_nowait(output_info)
+            output_info = []
+    except queue.Empty:
+        queue_for_get_calc_data.close()
+
+
 def write_to_csv(output_file, queue_for_calc_write_data):
     try:
-        if(os.path.exists(output_file)):
-            with open(output_file, "a") as output:
-                information = []
-                output_data = csv.writer(output, delimiter=",")
-                if not queue_for_calc_write_data.empty():
-                    while True:
-                        information = queue_for_calc_write_data.get(True)
-                        output_data.writerows(information)
-                print("Write data to csv process id: {0}, data is: {1}".format(os.getpid(), information))
-        else:
-            with open(output_file, "w") as output:
-                information = []
-                output_data = csv.writer(output, delimiter=",")
-                if not queue_for_calc_write_data.empty():
-                    while True:
-                        information = queue_for_calc_write_data.get(True)
-                        output_data.writerows(information)
-                print("Write data to csv process id: {0}, data is: {1}".format(os.getpid(), information))
-    except BaseException as e:
-        print("Exception: {0}".format(e))
-        sys.exit(0)
-
-
-def read_from_dict(
-        social_security_percent,
-        queue_for_get_calc_data,
-        queue_for_calc_write_data):
-    try:
-        output_info = []
-
-        baselow = float(social_security_percent["JiShuL"])
-        basehigh = float(social_security_percent["JiShuH"])
-        pension = float(social_security_percent["YangLao"])
-        medical = float(social_security_percent["YiLiao"])
-        unemployment = float(social_security_percent["ShiYe"])
-        injury = float(social_security_percent["GongShang"])
-        matermity = float(social_security_percent["ShengYu"])
-        provident = float(social_security_percent["GongJiJin"])
-
-        if not queue_for_get_calc_data.empty():
+        with open(output_file, "a") as output:
+            output_data = csv.writer(output, delimiter=",")
             while True:
-                before_salary_data = queue_for_get_calc_data.get(True)
-                print("receive salary data: {0}".format(before_salary_data))
-                job_number = before_salary_data[0]
-                before_salary = before_salary_data[1]
-
-                salary = get_salary(before_salary)
-
-                social_security = cal_social_security(
-                    baselow,
-                    basehigh,
-                    pension,
-                    medical,
-                    unemployment,
-                    injury,
-                    matermity,
-                    provident,
-                    salary)
-
-                taxable_income = cal_taxable_income(salary, social_security)
-                tax = calculator_tax(taxable_income)
-
-                after_salary = after_salary_tax(salary, social_security, tax)
-
-                social_security = Decimal(social_security).quantize(
-                    Decimal(".01"))
-                tax = Decimal(tax).quantize(Decimal(".01"))
-                after_salary = Decimal(after_salary).quantize(Decimal(".01"))
-
-                output_info = [
-                    job_number,
-                    salary,
-                    social_security,
-                    tax,
-                    after_salary]
-                queue_for_calc_write_data.put(output_info)
-                # time.sleep(random.random())
-        print("write_to_csv process id is: {0}, data is: {1}".format(os.getpid(), output_info))
-    except BaseException as e:
-        print(
-            "Please confirm your file path and file content: {0}".format(e))
+                information = queue_for_calc_write_data.get()
+                output_data.writerow(information)
+    except (BaseException, queue.Empty) as e:
+        print("write_to_csv func Exception: {0}".format(e))
+        queue_for_calc_write_data.close()
         sys.exit(0)
 
 
@@ -232,24 +191,21 @@ def main():
     social_security_percent, data_file, output_file = parsing_parameter(
         sys.argv[1:])
 
-    queue_for_get_calc_data = Queue()
-    queue_for_calc_write_data = Queue()
+    queue_for_get_calc_data = multiprocessing.Queue()
+    queue_for_calc_write_data = multiprocessing.Queue()
 
-    read_procs = Process(
-        target=process_data,
-        args=(data_file, 
-              queue_for_get_calc_data))
+    read_procs = multiprocessing.Process(
+        target=process_data, args=(
+            data_file, queue_for_get_calc_data))
 
-    calc_procs = Process(
-        target=read_from_dict,
-        args=(social_security_percent,
-              queue_for_get_calc_data,
-              queue_for_calc_write_data))
+    calc_procs = multiprocessing.Process(target=read_from_queue,
+                                         args=(social_security_percent,
+                                               queue_for_get_calc_data,
+                                               queue_for_calc_write_data))
 
-    write_procs = Process(
-        target=write_to_csv,
-        args=(output_file, 
-            queue_for_calc_write_data))
+    write_procs = multiprocessing.Process(
+        target=write_to_csv, args=(
+            output_file, queue_for_calc_write_data))
 
     read_procs.start()
     calc_procs.start()
@@ -258,7 +214,6 @@ def main():
     read_procs.join()
     calc_procs.join()
     write_procs.join()
-    print("Main Process {0}".format(os.getpid()))
 
 
 if __name__ == "__main__":
