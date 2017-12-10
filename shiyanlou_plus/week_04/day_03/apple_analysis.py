defb74bd81fc1b6f03c7c1c85a8e7d6f6d39d229
tongxindao <balin1991@qq.com>
2017-12-10 15:29:12 +0800

    temp commit

--- shiyanlou_plus/week_04/day_03/apple_analysis.py
+++ shiyanlou_plus/week_04/day_03/apple_analysis.py
@@ -3,25 +3,29 @@
 import pandas as pd
 
 
-def quarter_volume():
+def quarter_volume(index):
+    try: 
+        index = int(index)
+    except:
+        print("Please input number.")
+        exit(-1)
+
+    if index <= 0:
+        index = 1
+
     data = pd.read_csv("apple.csv", header=0)
+    data["Date"] = pd.to_datetime(data["Date"])
+    data = data.set_index("Date")
 
-    date = str(data.sort_values(by="Volume", ascending=False)[1:2]["Date"]).split()[1]
+    sort_data_by_quarter = data.resample("Q").sum().sort_values(
+            by="Volume", ascending=False)
 
-    year = int(date.split('-')[0])
-    month = int(date.split('-')[1])
-
-    if 1<= month <= 3:
-
-    elif 4<= month <= 6:
-
-    elif 7<= month <= 9:
-
-    elif 10<= month <= 12:
+    second_volume = int(str(sort_data_by_quarter[index-1:index]).split()[-1])
 
     return second_volume
 
 
 if __name__ == "__main__":
-    second_volume = quarter_volume()
+    index = input("Please input your want to know quarter sort index: ")
+    second_volume = quarter_volume(index)
     print("Second Volume: {0}".format(second_volume))
