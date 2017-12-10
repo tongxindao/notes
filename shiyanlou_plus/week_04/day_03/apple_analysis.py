d0fc495d5f5353f953c06d0840d745728a71312a
tongxindao <balin1991@qq.com>
2017-12-10 16:39:49 +0800

    temp commit

--- shiyanlou_plus/week_04/day_03/apple_analysis.py
+++ shiyanlou_plus/week_04/day_03/apple_analysis.py
@@ -1,5 +1,5 @@
 #_*_ coding:utf-8 _*_
-
+import time
 import pandas as pd
 
 
@@ -13,15 +13,22 @@ def quarter_volume(index):
     if index <= 0:
         index = 1
 
-    data = pd.read_csv("apple.csv", header=0)
-    data["Date"] = pd.to_datetime(data["Date"])
-    data = data.set_index("Date")
+    re_read_data = pd.read_csv("apple.csv", header=0, chunksize=5)
+    data_list = []
+    
+    for chunk in re_read_data:
+        data_list.append(chunk)
+
+    data = pd.concat(data_list)
+
+    data.Date = pd.to_datetime(data.Date)
+    data.index = data.Date
 
     sort_data_by_quarter = data.resample("Q").sum().sort_values(
             by="Volume", ascending=False)
 
     second_volume = int(str(sort_data_by_quarter[index-1:index]).split()[-1])
-
+    print(time.time())
     return second_volume
 
 
